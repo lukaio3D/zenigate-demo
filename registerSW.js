3e8cff30f821a95ca73df96eb4a300092759cd5b
Lukas Schaab <lukas.schaab@lukaio.de>
2025-09-09 21:16:01 +0200

    fixed issue with spa

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/zenigate-poc/sw.js', { scope: '/zenigate-poc/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
